acd907d490730f82e9a134c16412b2a0b3b121ed
Emlyn Corrin <emlyn@swiftkey.net>
2014-12-21 00:43:48 +0000

    My solver

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -7,5 +7,33 @@
                (slurp)
                (read-string)))
 
+(defn diff-chars [w1 w2]
+  (count (filter not (map = w1 w2))))
+
+(defn add-links [links word]
+  (reduce #(if (= 1 (diff-chars %2 word))
+             (-> %1
+                 (update-in [word] conj %2)
+                 (update-in [%2] conj word))
+             %1)
+          (assoc links word #{})
+          (keys links)))
+
+(defn build-links [words]
+  (reduce add-links {} words))
+
+;; If there are multiple solutions, this will just return one arbitrarily,
+;; not necessarily the shortest, but it seems to be enough to pass the tests.
 (defn doublets [word1 word2]
-  "make me work")
+  (let [n     (count word1)
+        links (build-links (filter #(= n (count %))
+                                   words))]
+    (letfn [(recurse [path next-words]
+              (when (seq next-words)
+                (if (next-words word2)
+                  (conj path word2)
+                  (first (remove nil?
+                                 (for [w (remove (set path) next-words)]
+                                   (recurse (conj path w) (links w))))))))]
+      (or (recurse [word1] (links word1))
+          []))))
